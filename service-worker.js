b1f1c7e527164d044dd9a675284caae5f48a4f06
awgdj26gdp-cmd <awgdj26gdp@gmail.com>
2026-02-14 23:40:37 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "penguin-v1";
+const CACHE_NAME = "penguin";
 const ASSETS = ["./", "./index.html", "./script.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
