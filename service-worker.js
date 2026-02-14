8809bbf18fd1639828038cd1f4d594ada060dfbb
awgdj26gdp-cmd <awgdj26gdp@gmail.com>
2026-02-15 02:00:06 +0900

    Update cache name and add style.css to assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "penguin-v2";
-const ASSETS = ["./", "./index.html", "./script.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "penguin-v4";
+const ASSETS = ["./", "./index.html", "./script.js", "./style.css", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
